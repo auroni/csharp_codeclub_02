be20d395d6f49145f4a0ef3b2ac0c6616f78fc9e
auroni <joonas.hyttinen@gmail.com>
2015-09-16 18:02:53 +0300

    myllyt sisään

---
+++ ConsoleApplication1/Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Player
+    {
+        private string name = "";
+        private int hitpoints = 0;
+        private int attack = 0;
+
+        public Player(string givenName)
+        {
+            name = givenName;
+            hitpoints = 100;
+        }
+
+        public void TakeDamage()
+        {
+            hitpoints = hitpoints - 10;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Current health: " + hitpoints);
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int HitPoints
+        {
+            get
+            {
+                return hitpoints;
+            }
+            set
+            {
+                hitpoints = value;
+            }
+        }
+        public int Attack
+        {
+            get
+            {
+                return attack;
+            }
+            set
+            {
+                attack = value;
+            }
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player playerOne = new Player("Marko");
+            Player playerTwo = new Player("JABE");
+
+            string option = "Continue";
+            while (option != "Quit")
+            {
+
+            }
+            
+        }
+    }
+}
